48f72f5ca3da994e75a7b1a691fbf9e31e3dfabc
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-09-27 11:34:25 +0100

    Before TS demo

---
+++ mod-09/demo.js
@@ -0,0 +1,21 @@
+class Calculator {
+  constructor(element) {
+    this.element = element;
+  }
+
+  add(x, y) {
+    const sum = x + y;
+    this.element.textContent = sum;
+    return sum;
+  }
+
+  subtract(x, y) {
+    const diff = x - y;
+    this.element.textContent = diff;
+    return diff;
+  }
+}
+
+const calc = new Calculator(document.getElementById('result'));
+console.log('1 + 2 =', calc.add(1, 2));
+console.log('10 - 5 =', calc.subtract(10, 5));
